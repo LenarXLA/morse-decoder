ff404b9e7661081d91547dd8f4d7a3488a57607d
Lenar <leonruscraft@gmail.com>
2020-10-20 18:17:25 +0300

    feat: solve morse-decoder

--- src/index.js
+++ src/index.js
@@ -35,10 +35,35 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '|space|':  ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+  
+    let regexpSpl = /.{10}/g;
+  
+    let regexp11 = /(11)/g;
+    let regexp10 = /(10)/g;
+    let regexpSpace = /(\*\*\*\*\*\*\*\*\*\*)/g;
+    let regexpZeros = /(0)/g;
+  
+    let start = expr.replace(regexpSpl, function(a) { return a + ' ' });
+        
+    let newstr11 = start.replace(regexp11, '-');
+    let newstr12 = newstr11.replace(regexp10, '.');
+    let newstrNotSpaces = newstr12.replace(regexpSpace, ' |space| ');
+    let newstrNotZeros = newstrNotSpaces.replace(regexpZeros, ' ');
+    
+    let arr = newstrNotZeros.split(' ');
+  
+    for(let elem of arr) {
+      if(elem !== '') {
+        result += MORSE_TABLE[elem];
+      }
+    }
+ 
+    return result;
 }
 
 module.exports = {
